2782ba43da601f5491d9365ee78bb39fbed935fa
Ahmed <ahmedhatem.aez@gmail.com>
2025-04-26 15:56:35 +0300

    Add functions to get temprature and convert between degrees

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 
+function extracting_data(data) {
+    console.log("hello");
+    //const data = JSON.parse(json_data);
+    celsius(data.currentConditions.temp);
+}
+
+function celsius(fahr) {
+    console.log(Math.round((fahr - 32) * (5 / 9)));
+}
 
 async function get_weather_data(location) {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=9HMH4KQN2L8F9DNCS7VT2DLLW&contentType=json`;
     try {
-        const response = await fetch(url, { mode: "cors" })
-        const json = response.json();
-        console.log(json);
+        const response = await fetch(url, { mode: "cors" });
+        const data = await response.json();
+        extracting_data(data);
     }
-    catch {
-
+    catch (error) {
+        console.error(error.message);
     }
 }
 
-async function api_hit(url) {
 
-}
+
 
 get_weather_data("London");
-//api_hit(url);
